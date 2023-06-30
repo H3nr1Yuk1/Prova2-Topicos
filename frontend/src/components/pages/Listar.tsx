8ddb2ed2c628fae3faacfe4f8180d1b1c87b2210
H3nr1Yuk1 <mathsilvaesilva@gmail.com>
2023-06-30 11:01:58 -0300

    Front

--- frontend/src/components/pages/Listar.tsx
+++ frontend/src/components/pages/Listar.tsx
@@ -1,67 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import { Despesas } from "../../models/despesas.model";
 
 function Listar() {
-    const [despesas, setDespesa] = useState([]);
-  
-    function carregarDados() {
-      axios
-        .get("http://localhost:3001")
-        .then((resposta) => {
-          setDespesa(resposta.data.dados);
-        })
-        .catch((erro) => {
-          console.log(erro);
-        });
-    }
-  
+    const [despesas, setDespesa] = useState<Despesas[]>([]);
+
     useEffect(() => {
-      carregarDados();
-    }, []);
-  
-    function remover(id: number) {
       axios
-        .delete("http://localhost:3001/" + id)
-        .then((resposta) => {
-          carregarDados();
-        })
-        .catch((erro) => {
-          // console.log(erro);
-        });
-    }
+      .get("http://localhost:3333/listar")
+      .then((resposta) => {
+          setDespesa(resposta.data.lista);
+      })
+      .catch((erro) => {
+        console.log(erro);
+      });
+    })
   
     return (
       <div>
-        <h1>Listagem de Despesas</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Descrição</th>
-              <th>Preço</th>
-              <th>Detalhes</th>
-              <th>Remover</th>
-            </tr>
-          </thead>
-          <tbody>
-            {despesas.map((produto: any) => (
-              <tr>
-                <td>{despesas.id}</td>
-                <td>{despesas.descricao}</td>
-                <td>{despesas.preco}</td>
-                <td>
-                  {/* <Link to={"/detalhes/" + produto.id}> Detalhes </Link> */}
-                  <Link to={`/detalhar/${despesas.id}`}> Detalhes </Link>
-                </td>
-                <td>
-                  <button onClick={() => remover(despesas.id)}>Remover</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <center>
+            <h1>Listagem de Despesas</h1>
+            <table>
+            <thead>
+                <tr>
+                <th>#</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                </tr>
+            </thead>
+            <tbody>
+                {despesas.map((despesa: any) => (
+                <tr>
+                    <td>{despesa.id}</td>
+                    <td>{despesa.descricao}</td>
+                    <td>R$ {despesa.preco}</td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        </center>
       </div>
     );
   }
--- frontend/src/components/pages/Visualizar.tsx
+++ frontend/src/components/pages/Visualizar.tsx
@@ -1,7 +1,42 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import styles from './vizualizar.module.css'
+
 function Vizualizar(){
+    const [total, setTotal] = useState()
+    const [media, setMedia] = useState()
+
+    useEffect(() => {
+        axios
+        .get("http://localhost:3333/calcular")
+        .then((resposta) => {
+            setTotal(resposta.data.total)
+            setMedia(resposta.data.media)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    })
+
     return (
         <div>
-
+            <center>
+                <h1>Total e média das despesas</h1>
+                <table>
+                    <tr>
+                        <td>Total:</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.resultado}>R$ {total}</td>
+                    </tr>
+                    <tr>
+                        <td>Média</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.resultado}>R$ {media}</td>
+                    </tr>
+                </table>
+            </center>
         </div>
     )
 }
